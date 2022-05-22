498aa9f4551b05e7269ce7afe7a8b36c9e61d300
MohsenSedaghat <38179125+MohsenSedaghat@users.noreply.github.com>
2022-05-22 16:38:38 +0430

    add McGee ATP

--- Function-HttpTrigger-Transform-Inv-To-SharePoint.cs
+++ Function-HttpTrigger-Transform-Inv-To-SharePoint.cs
@@ -146,6 +146,38 @@ namespace MHC.Function
                     sharePointResultObject.Status = inventoryInputObject.status;
                     sharePointResultObject.FileName = inventoryInputObject.CardCode + "_" + lineItem.InventoryLine.VendorPartNumber;
                 }
+                 else if (inventoryInputObject.CardCode == "CUS002317")
+                {
+                    LineItem lineItem = JsonConvert.DeserializeObject<LineItem>(inventoryInputObject.InventoryContent.ToString());
+                    sharePointResultObject.SKU = lineItem.InventoryLine.VendorPartNumber;
+                    sharePointResultObject.CardCode = inventoryInputObject.CardCode;
+                    sharePointResultObject.Atp = lineItem.QuantitiesSchedulesLocations.Find(x => x.QuantityQualifier == "33")?.TotalQty ?? 0;
+                    sharePointResultObject.AtpDate = lineItem.QuantitiesSchedulesLocations.Find(x => x.QuantityQualifier == "33")?.Dates.Date;
+                    sharePointResultObject.QuantityOnOrder = lineItem.QuantitiesSchedulesLocations.Find(x => x.QuantityQualifier == "29")?.TotalQty ?? 0; ;
+                    sharePointResultObject.QuantityOnOrderDate = lineItem.QuantitiesSchedulesLocations.Find(x => x.QuantityQualifier == "29")?.Dates.Date;
+                    sharePointResultObject.DeliveryMode = inventoryInputObject.DeliveryMode;
+                    sharePointResultObject.PartnerName = "SPS_McGee";
+                    sharePointResultObject.Warehouse = "";
+                    sharePointResultObject.Content = inventoryInputObject.InventoryContent;
+                    sharePointResultObject.Status = inventoryInputObject.status;
+                    sharePointResultObject.FileName = inventoryInputObject.CardCode + "_" + lineItem.InventoryLine.VendorPartNumber;
+                }
+                 else if (inventoryInputObject.CardCode == "CUS002317-ATP")
+                {
+                    LineItem lineItem = JsonConvert.DeserializeObject<LineItem>(inventoryInputObject.InventoryContent.ToString());
+                    sharePointResultObject.SKU = lineItem.InventoryLine.VendorPartNumber;
+                    sharePointResultObject.CardCode = inventoryInputObject.CardCode;
+                    sharePointResultObject.Atp = lineItem.QuantitiesSchedulesLocations.Find(x => x.QuantityQualifier == "33")?.TotalQty ?? 0;
+                    sharePointResultObject.AtpDate = lineItem.QuantitiesSchedulesLocations.Find(x => x.QuantityQualifier == "33")?.Dates.Date;
+                    sharePointResultObject.QuantityOnOrder = lineItem.QuantitiesSchedulesLocations.Find(x => x.QuantityQualifier == "29")?.TotalQty ?? 0; ;
+                    sharePointResultObject.QuantityOnOrderDate = lineItem.QuantitiesSchedulesLocations.Find(x => x.QuantityQualifier == "29")?.Dates.Date;
+                    sharePointResultObject.DeliveryMode = inventoryInputObject.DeliveryMode;
+                    sharePointResultObject.PartnerName = "SPS_McGee";
+                    sharePointResultObject.Warehouse = "";
+                    sharePointResultObject.Content = inventoryInputObject.InventoryContent;
+                    sharePointResultObject.Status = inventoryInputObject.status;
+                    sharePointResultObject.FileName = inventoryInputObject.CardCode + "_" + lineItem.InventoryLine.VendorPartNumber;
+                }
                 else if (inventoryInputObject.CardCode == "CUS001607")
                 {
                     LineItem lineItem = JsonConvert.DeserializeObject<LineItem>(inventoryInputObject.InventoryContent.ToString());
