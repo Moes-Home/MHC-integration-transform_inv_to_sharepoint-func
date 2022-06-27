d9d3e713f1dc220707cb6be0c860d78606fa9c54
MohsenSedaghat <38179125+MohsenSedaghat@users.noreply.github.com>
2022-06-27 20:03:01 +0430

    add Target.com

--- Function-HttpTrigger-Transform-Inv-To-SharePoint.cs
+++ Function-HttpTrigger-Transform-Inv-To-SharePoint.cs
@@ -226,6 +226,22 @@ namespace MHC.Function
                     sharePointResultObject.Status = inventoryInputObject.status;
                     sharePointResultObject.FileName = inventoryInputObject.CardCode + "_" + lineItem.InventoryLine.VendorPartNumber;
                 }
+                 else if (inventoryInputObject.CardCode == "CUS003941-ATP")
+                {
+                    LineItem lineItem = JsonConvert.DeserializeObject<LineItem>(inventoryInputObject.InventoryContent.ToString());
+                    sharePointResultObject.SKU = lineItem.InventoryLine.VendorPartNumber;
+                    sharePointResultObject.CardCode = inventoryInputObject.CardCode;
+                    sharePointResultObject.Atp = lineItem.QuantitiesSchedulesLocations.Find(x => x.QuantityQualifier == "33")?.TotalQty ?? 0;
+                    sharePointResultObject.AtpDate = lineItem.QuantitiesSchedulesLocations.Find(x => x.QuantityQualifier == "33")?.Dates.Date;
+                    sharePointResultObject.QuantityOnOrder = lineItem.QuantitiesSchedulesLocations.Find(x => x.QuantityQualifier == "29")?.TotalQty ?? 0; ;
+                    sharePointResultObject.QuantityOnOrderDate = lineItem.QuantitiesSchedulesLocations.Find(x => x.QuantityQualifier == "29")?.Dates.Date;
+                    sharePointResultObject.DeliveryMode = inventoryInputObject.DeliveryMode;
+                    sharePointResultObject.PartnerName = "SPS_Target.com";
+                    sharePointResultObject.Warehouse = "";
+                    sharePointResultObject.Content = inventoryInputObject.InventoryContent;
+                    sharePointResultObject.Status = inventoryInputObject.status;
+                    sharePointResultObject.FileName = inventoryInputObject.CardCode + "_" + lineItem.InventoryLine.VendorPartNumber;
+                }
                 else if (inventoryInputObject.CardCode == "CCAN000289")
                 {
                     InventoryUpdate inventoryUpdate = JsonConvert.DeserializeObject<InventoryUpdate>(inventoryInputObject.InventoryContent.ToString());
